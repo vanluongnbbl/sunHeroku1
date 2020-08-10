485b4ae14a53648d35f3505895ca0f38817d738d
luong <vanluongnbbl@gmail.com>
2020-08-10 15:00:42 +0700

    f2

--- login.php
+++ login.php
@@ -2,7 +2,7 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Machine Company</title>
+  <title>Login</title>
   <link rel="stylesheet" href="./css/responsive.css">
   <link rel="stylesheet" href="./css/header.css">
   <link rel="stylesheet" href="./css/style.css">
